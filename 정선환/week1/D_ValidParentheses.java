683eb3f73e664259982735b2f08a39475c14ffbc
znzn <znzn9292@gmail.com>
2024-01-07 10:22:41 +0900

    Add: 유효한 괄호 풀이

---
+++ 정선환/week1/D_ValidParentheses.java
@@ -0,0 +1,49 @@
+package week1;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+/**
+ * Valid Parentheses (유효한 괄호)
+ *
+ * 대중소 세류 괄호로 된 입력값이 유효한지 판별하라
+ * 입력: []{}()
+ * 출력: true
+ *
+ * https://leetcode.com/problems/valid-parentheses/
+ */
+public class D_ValidParentheses {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+
+        isValid(input);
+    }
+
+    public static boolean isValid(String s) {
+        char[] charArray = s.toCharArray();
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : charArray) {
+            if(c == '(') {
+                stack.push(')');
+
+            }else if(c == '{') {
+                stack.push('}');
+
+            }else if(c == '[') {
+                stack.push(']');
+
+            }else {
+                if(stack.isEmpty() || c != stack.pop().charValue()) {
+                    return false;
+                }
+            }
+        }
+        if(stack.isEmpty()) {
+            return true;
+        }
+
+        return false;
+    }
+}
