bf4e1870889546bccb85e22edeb057f396e46aac
jeongseonhwan <znzn9292@gmail.com>
2024-01-10 18:10:52 +0900

    Refactor: 원점에서 가장 가까운 k개의 점

---
+++ 정선환/week1/H_KClosetPointsToOrigin.java
@@ -0,0 +1,11 @@
+package week1;
+
+public class H_KClosetPointsToOrigin {
+    public static void main(String[] args) {
+
+    }
+
+    public int[][] kClosest(int[][] points, int k) {
+        return null;
+    }
+}
