4b883bd91c80a5ffe3787c5aca257dfb44d89ccb
znzn <znzn9292@gmail.com>
2024-01-16 00:54:19 +0900

    Add: Graph DFS

--- 정선환/week2/E_NumberOfIslands.java
+++ 정선환/week2/E_NumberOfIslands.java
@@ -1,22 +1,41 @@
 package week2;
 
+/**
+ * https://leetcode.com/problems/number-of-islands/description/
+ */
 public class E_NumberOfIslands {
+
+    static int m;
+    static int n;
+    static boolean[][] visited;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+
+
     public static void main(String[] args) {
-//        char[][] graph = {
-//                {'1', '1', '1', '1', '0'},
-//                {'1', '1', '0', '1', '0'},
-//                {'1', '1', '0', '0', '0'},
-//                {'0', '0', '0', '0', '0'}
-//        };
-    }
+        char[][] graph = {
+                {'1', '1', '1', '1', '0'},
+                {'1', '1', '0', '1', '0'},
+                {'1', '1', '0', '0', '0'},
+                {'0', '0', '0', '0', '0'}
+        };
 
-    public static void bfs() {
-
+        numIslands(graph);
     }
 
     public static int numIslands(char[][] grid) {
+        m = grid.length;
+        n = grid[0].length;
+        visited = new boolean[m][n];
+
+        for(int i = 0; i < m; i++) {
+            for (int j = 0; j < n; i++) {
+                if(grid[i][j] == '1' && visited[i][j] == false) {
+
+                }
+            }
+        }
         return 0;
     }
-
 
 }
