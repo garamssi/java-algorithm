6e6966d98405944e1675d9d591a1dfdc0923e60d
jeongseonhwan <znzn9292@gmail.com>
2024-01-16 18:00:10 +0900

    Refactor: 섬의 개수 풀이

--- 정선환/week2/E_NumberOfIslands.java
+++ 정선환/week2/E_NumberOfIslands.java
@@ -1,5 +1,8 @@
 package week2;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * https://leetcode.com/problems/number-of-islands/description/
  */
@@ -13,14 +16,22 @@ public class E_NumberOfIslands {
 
 
     public static void main(String[] args) {
+//        char[][] graph = {
+//                {'1', '1', '1', '1', '0'},
+//                {'1', '1', '0', '1', '0'},
+//                {'1', '1', '0', '0', '0'},
+//                {'0', '0', '0', '0', '0'}
+//        };
+
         char[][] graph = {
-                {'1', '1', '1', '1', '0'},
-                {'1', '1', '0', '1', '0'},
                 {'1', '1', '0', '0', '0'},
-                {'0', '0', '0', '0', '0'}
+                {'1', '1', '0', '0', '0'},
+                {'0', '0', '1', '0', '0'},
+                {'0', '0', '0', '1', '1'}
         };
 
-        numIslands(graph);
+        System.out.println(numIslands(graph));
+
     }
 
     public static int numIslands(char[][] grid) {
@@ -28,14 +39,42 @@ public class E_NumberOfIslands {
         n = grid[0].length;
         visited = new boolean[m][n];
 
+        int numberOfIsland = 0;
         for(int i = 0; i < m; i++) {
-            for (int j = 0; j < n; i++) {
+            for (int j = 0; j < n; j++) {
                 if(grid[i][j] == '1' && visited[i][j] == false) {
-
+                    numberOfIsland += bfs(grid, i, j);
                 }
             }
         }
-        return 0;
+        return numberOfIsland;
     }
 
+    public static int bfs(char[][] graph, int x, int y) {
+        visited[x][y] = true;
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{x, y});
+
+        while(!queue.isEmpty()) {
+            int[] current = queue.poll();
+
+            for(int i = 0; i < 4; i++) {
+                int nextX = current[0] + dx[i];
+                int nextY = current[1] + dy[i];
+
+                // 방문하면 안되는 조건
+                // 1. m,n 을 넘어서면 안됨
+                // 2. 방문했던 좌표는 제외
+                // 3. 물인 좌표 제외
+                if(nextX >= 0 && nextX < m && nextY >= 0 && nextY < n) {
+                    if(!visited[nextX][nextY] && graph[nextX][nextY] == '1') {
+                        visited[nextX][nextY] = true;
+                        queue.offer(new int[]{nextX, nextY});
+                    }
+                }
+            }
+        }
+
+        return 1;
+    }
 }
