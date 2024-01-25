8823b26eacd40ff9c75eea9ff3a79c9157bf99d6
JKjiwon <jwkim.oa@gmail.com>
2024-01-25 20:05:49 +0900

    [김지원] greatest-common-divisor-of-strings 리펙토링

--- 김지원/week04/02.greatest-common-divisor-of-strings/Solution.java
+++ 김지원/week04/02.greatest-common-divisor-of-strings/Solution.java
@@ -1,28 +1,38 @@
 class Solution {
+    /*
+    * "t divides s" => s = t + ... + t
 
+    if (x dievides str1, str2)
+    str1 = x + ... + x
+    str2 = x + ... + x
+
+    gcd(ABABAB, ABAB) = AB
+    ABABAB = AB + AB + AB
+    ABAB = AB + AB
+
+    gcd(6, 4) = 2
+    6 = 2 + 2 + 2
+    4 = 2 + 2
+    */
     public String gcdOfStrings(String str1, String str2) {
         // str1, str2의 gcd가 없는 경우(즉 완전히 다른 문자)
+        // if (x dievides str1, str2) => false
         if (!(str1 + str2).equals(str2 + str1)) return "";
 
+        // if (x dievides str1, str2) => true
         int gcsRs = gcd(str1.length(), str2.length());
         return str1.substring(0, gcsRs);
     }
-
+    
+    // 유클리드 호제법을 이용한 gcd 계산
+    //a % b 연산을 하여 b가 0이 될때까지, 반복문을 돌게 되고, b가 0인 순간의 a를 GCD로 판단
     private int gcd(int a, int b) {
-        int tmp, n;
+        int tmp;
 
-        //a에 큰 값을 위치시키기 위한 조건이다.
-        if (a < b) {
-            tmp = a;
+        while (b != 0) {
+            tmp = a % b;
             a = b;
             b = tmp;
-        }
-
-        //b가 0이 될때까지(a%b), 반복문을 돌게 되고, b가 0인 순간의 a를 GCD로 판단하고 리턴한다
-        while (b != 0) {
-            n = a % b;
-            a = b;
-            b = n;
         }
         return a;
     }
