12bd69aa9a96d2873ac0bde0da5621e819c4e985
JKjiwon <jwkim.oa@gmail.com>
2024-01-28 09:48:05 +0900

    is-subsequence 조건 변경

--- 김지원/week04/05.is-subsequence/Solution.java
+++ 김지원/week04/05.is-subsequence/Solution.java
@@ -4,7 +4,7 @@ class Solution {
         int sIndex = 0;
 
         for (int i = 0; i < t.length(); i++) {
-            if (sIndex >= s.length()) {
+            if (sIndex == s.length()) {
                 return true;
             }
 
