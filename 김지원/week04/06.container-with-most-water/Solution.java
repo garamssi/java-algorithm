3da258c81efe9f319d7ba377b97bb437ac48421a
JKjiwon <jwkim.oa@gmail.com>
2024-01-28 09:38:52 +0900

    container-with-most-water 변수명 수정

--- 김지원/week04/06.container-with-most-water/Solution.java
+++ 김지원/week04/06.container-with-most-water/Solution.java
@@ -1,12 +1,12 @@
 class Solution {
 
     public int maxArea(int[] height) {
-        int maxWater = 0;
+        int maxArea = 0;
         int left = 0;
         int right = height.length - 1;
         
         while (left < right) {
-            maxWater = Math.max(maxWater, calStoredWater(height, left, right));
+            maxArea = Math.max(maxArea, calStoredWater(height, left, right));
             // 새 경계의 높이가 더 큰 경우에만 컨테이너 너비를 줄이면 영역이 더 커질 수 있다.
             // 더 넓은 영역이 나올 수 있는 가능성이 있는 방향으로 포인터가 움직인다.
             if (height[left] < height[right]) {
@@ -15,7 +15,7 @@ class Solution {
                 right--;
             }
         }
-        return maxWater;
+        return maxArea;
     }
 
     private int calStoredWater(int[] height, int i, int j) {
