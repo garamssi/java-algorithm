192325ba1b60e91147de294c2250f2b885b542c9
JKjiwon <jwkim.oa@gmail.com>
2024-01-28 09:37:02 +0900

    container-with-most-water 주석 수정

--- 김지원/week04/06.container-with-most-water/Solution.java
+++ 김지원/week04/06.container-with-most-water/Solution.java
@@ -7,7 +7,7 @@ class Solution {
         
         while (left < right) {
             maxWater = Math.max(maxWater, calStoredWater(height, left, right));
-            // 새 경계의 높이가 더 큰 경우에만 컨테이너 너비를 늘리면 영역이 더 커질 수 있다.
+            // 새 경계의 높이가 더 큰 경우에만 컨테이너 너비를 줄이면 영역이 더 커질 수 있다.
             // 더 넓은 영역이 나올 수 있는 가능성이 있는 방향으로 포인터가 움직인다.
             if (height[left] < height[right]) {
                 left++;
