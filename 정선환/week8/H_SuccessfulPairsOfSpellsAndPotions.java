291fbcc6b924c2ae8a5543bb0ac9ba9992b4d738
jeongseonhwan <znzn9292@gmail.com>
2024-02-27 17:43:24 +0900

    Add: Successful Pairs of Spells and Potions 풀이

--- 정선환/week8/H_SuccessfulPairsOfSpellsAndPotions.java
+++ 정선환/week8/H_SuccessfulPairsOfSpellsAndPotions.java
@@ -6,10 +6,11 @@ import java.util.Arrays;
  * https://www.google.com/search?q=%EA%B5%AC%EA%B8%80%EB%B2%88%EC%97%AD&oq=%EA%B5%AC%EA%B8%80%EB%B2%88%EC%97%AD&gs_lcrp=EgZjaHJvbWUqBggAEEUYOzIGCAAQRRg70gEIMzgwMmowajGoAgCwAgA&sourceid=chrome&ie=UTF-8
  * 2300. Successful Pairs of Spells and Potions
  */
-public class E_SuccessfulPairsOfSpellsAndPotions {
+public class H_SuccessfulPairsOfSpellsAndPotions {
     public static void main(String[] args) {
         int[] spells = {3,1,2};
-        int[] potions = {8,5,8};
+        int[] potions = {8,5,2,1,6,7};  // 1,2,5,6,7,8
+
         long success = 16;
 
         int[] answer = successfulPairs(spells, potions, success);
@@ -38,27 +39,28 @@ public class E_SuccessfulPairsOfSpellsAndPotions {
 //    }
 
     public static int[] successfulPairs(int[] spells, int[] potions, long success) {
-        Arrays.sort(spells);
+        Arrays.sort(potions);
 
-        int length = spells.length;
-        int[] pair = new int[length];
+        int spellLength = spells.length;
+        int[] pair = new int[spellLength];
 
-        for(int i = 0; i < length; i++) {
-            pair[i] = binarySearch(potions, spells[i], success);
+        for(int i = 0; i < spellLength; i++) {
+            int minRange = 0;
+            int maxRange = potions.length - 1;
+
+            while(minRange <= maxRange) {
+                int mid = minRange + (maxRange - minRange) / 2;
+
+                if((long) potions[mid] * spells[i] >= success) {
+                    maxRange = mid - 1;
+                }else {
+                    minRange = mid + 1;
+                }
+            }
+
+            pair[i] = potions.length - minRange;
         }
 
         return pair;
     }
-
-    public static int binarySearch(int[] potions, int spell, long success) {
-        int minRange = 0;
-        int maxRange = potions.length;
-
-        while(minRange <= maxRange) {
-            
-        }
-
-    }
-
-
 }
