8bcd4c06b53a2128f28e8d06aa0bfce22ce31fb5
jeongseonhwan <znzn9292@gmail.com>
2024-03-07 18:13:59 +0900

    Add: 다리를지나는트럭 풀이 중

--- 정선환/week10/C_다리를지나는트럭.java
+++ 정선환/week10/C_다리를지나는트럭.java
@@ -13,21 +13,30 @@ public class C_다리를지나는트럭 {
     public static int solution(int bridge_length, int weight, int[] truck_weights) {
 
         // 큐 자료구조 초기화
-        Queue<Integer> waitTruckQueue = new LinkedList<>();
+        Queue<Truck> waitTruckQueue = new LinkedList<>();
         for(int i = 0; i < truck_weights.length; i++) {
-            waitTruckQueue.offer(truck_weights[i]);
+            Truck truck = new Truck(i, truck_weights[i]);
+            waitTruckQueue.offer(truck);
         }
-        Queue<Integer> crossingTruckQueue = new LinkedList<>();
-        Queue<Integer> crossedTruckQueue = new LinkedList<>();
+        Queue<Truck> crossingTruckQueue = new LinkedList<>();
+        Queue<Truck> crossedTruckQueue = new LinkedList<>();
+
+        // 트럭 이동 중인 map
+        Map<Truck, Integer> crossingTruckMap = new HashMap<>();
 
         int time = 0;
         while(!waitTruckQueue.isEmpty() && crossedTruckQueue.size() != truck_weights.length) {
             time++;
 
+            // 다리를 건너는 트럭 -> 다리를 지난 트럭
+
+
             // 대기트럭 -> 다리를 건너는 트럭
-            int crossingTruckSum = crossingTruckQueue.stream().mapToInt(Integer::intValue).sum();
+            int crossingTruckSum = crossingTruckQueue.stream().mapToInt(Truck::getWeight).sum();
             if(crossingTruckQueue.size() < bridge_length && crossingTruckSum < weight) {
-                crossingTruckQueue.offer(waitTruckQueue.poll());
+                Truck truck = waitTruckQueue.poll();
+                crossingTruckQueue.offer(truck);
+                crossingTruckMap.put(truck, 1);
             }
 
 
@@ -35,4 +44,23 @@ public class C_다리를지나는트럭 {
 
         return 0;
     }
+
+    public static class Truck {
+        int number;
+        int weight;
+
+        public Truck(int number, int weight) {
+            this.number = number;
+            this.weight = weight;
+        }
+
+        public int getNumber() {
+            return number;
+        }
+
+        public int getWeight() {
+            return weight;
+        }
+    }
+
 }
