68029629928b9401ffc597a999fb4d39178596bf
jeongseonhwan <znzn9292@gmail.com>
2024-03-14 18:01:08 +0900

    Add: 디스크 컨트롤러 풀이

---
+++ 정선환/week11/B_디스크컨트롤러.java
@@ -0,0 +1,16 @@
+package week11;
+
+/**
+ * https://school.programmers.co.kr/learn/courses/30/lessons/42627
+ * 디스크 컨트롤러
+ */
+public class B_디스크컨트롤러 {
+    public static void main(String[] args) {
+
+    }
+
+    public static int solution(int[][] jobs) {
+        int answer = 0;
+        return answer;
+    }
+}
