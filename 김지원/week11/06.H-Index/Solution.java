6b8ad194cc86038ca973279b0ca1998052319812
Jiwon Kim <jwkim.oa@gmail.com>
2024-03-19 21:57:05 +0900

    [김지원] week11 hIndex refactoring

--- 김지원/week11/06.H-Index/Solution.java
+++ 김지원/week11/06.H-Index/Solution.java
@@ -3,18 +3,17 @@ import java.util.Arrays;
 class Solution {
     public static void main(String[] args) {
         int[] cn = {3, 0, 6, 1, 5};
-        // {0, 1, 3, 5, 6}
         int rs = new Solution().solution(cn);
         System.out.println(rs);
     }
 
     public int solution(int[] citations) {
         int answer = 0;
-        Arrays.sort(citations);
+        Arrays.sort(citations); // 정렬
 
-        for (int i = 1; i <= citations.length; i++) {
-            if (citations[citations.length - i] >= i) {
-                answer = i;
+        for (int hIndex = 1; hIndex <= citations.length; hIndex++) { 
+            if (citations[citations.length - hIndex] >= hIndex) {
+                answer = hIndex;
             }
         }
         return answer;
