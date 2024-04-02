6557dc8c803008cf764c30195d53d2a138fa7efc
jeongseonhwan <znzn9292@gmail.com>
2024-04-02 17:59:42 +0900

    Add: 조이스틱 풀이 중

---
+++ 정선환/week13/E_조이스틱.java
@@ -0,0 +1,21 @@
+package week13;
+
+/**
+ * https://school.programmers.co.kr/learn/courses/30/lessons/42860
+ * 조이스틱
+ */
+public class E_조이스틱 {
+    public static void main(String[] args) {
+        System.out.println(solution("JEROEN")); // 56
+        System.out.println(solution("JAN")); // 23
+    }
+
+    public static int solution(String name) {
+        int answer = 0;
+
+        
+
+
+        return answer;
+    }
+}
