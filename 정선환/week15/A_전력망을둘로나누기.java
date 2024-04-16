c129997a4fac9c4fde3eebdabe4948850669ca90
jeongseonhwan <znzn9292@gmail.com>
2024-04-16 17:52:56 +0900

    Add: 전력망을 둘로 나누기 풀이 중

---
+++ 정선환/week15/A_전력망을둘로나누기.java
@@ -0,0 +1,16 @@
+package week15;
+
+/**
+ * https://school.programmers.co.kr/learn/courses/30/lessons/86971
+ * 전력망을 둘로 나누기
+ */
+public class A_전력망을둘로나누기 {
+    public static void main(String[] args) {
+        solution(4, new int[][]{{1,2},{2,3},{3,4}});
+    }
+
+    public static int solution(int n, int[][] wires) {
+        int answer = -1;
+        return answer;
+    }
+}
