0f2be3f44046745a757714b749b953432ce5e4a8
jeongseonhwan <znzn9292@gmail.com>
2024-05-14 18:02:51 +0900

    Add: 덩치 풀이

---
+++ 정선환/week18/C_덩치.java
@@ -0,0 +1,27 @@
+package week18;
+
+/**
+ * https://www.acmicpc.net/problem/7568
+ * 덩치
+ */
+public class C_덩치 {
+    public static void main(String[] args) {
+        int[][] arr = {
+                {55, 185},
+                {58, 183},
+                {88, 186},
+                {60, 175},
+                {46, 155}
+            };
+
+        int[] result = solution(arr);
+        for(int i = 0; i < result.length; i++) {
+            System.out.print(result[i] + " ");
+        }
+    }
+
+    public static int[] solution(int[][] arr) {
+
+        return null;
+    }
+}
