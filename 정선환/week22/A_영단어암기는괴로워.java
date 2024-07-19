8ac6251a988a9f1401ad3165af2c5d7e0da7c49c
znzn9292 <znzn9292@gmail.com>
2024-07-19 10:57:15 +0900

    Update: 영단어 암기는 괴로워 풀이

--- 정선환/week22/A_영단어암기는괴로워.java
+++ 정선환/week22/A_영단어암기는괴로워.java
@@ -23,6 +23,28 @@ public class A_영단어암기는괴로워 {
             }
         }
 
+        ArrayList<Map.Entry<String, Integer>> list = new ArrayList<>(map.entrySet());
+        list.sort(new Comparator<Map.Entry<String, Integer>>() {
+            @Override
+            public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+                // 1.자주 나오는 단어일수록 앞에 배치
+                int frequencyComparison = o2.getValue().compareTo(o1.getValue());
+
+                if (frequencyComparison != 0) {
+                    return frequencyComparison;
+                }
+
+                // 2.해당 단어의 길이가 길수록 앞에 배치
+                int lengthComparison = Integer.compare(o2.getKey().length(), o1.getKey().length());
+                if (lengthComparison != 0) {
+                    return lengthComparison;
+                }
+
+                // 3.알파벳 사전 순으로 앞에 있는 단어일수록 앞에 배치
+                return o1.getKey().compareTo(o2.getKey());
+            }
+        });
+
         for (Map.Entry<String, Integer> answerMap : list) {
             bw.write(answerMap.getKey()+ "\n");
         }
