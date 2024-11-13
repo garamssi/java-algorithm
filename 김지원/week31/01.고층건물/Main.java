044e955d65aab8a8ab7cf2c22c26bb8b26f0b4bd
kimjiwon <jwkim.oa@gmail.com>
2024-11-13 14:11:30 +0900

    week30 풀이 수정

--- 김지원/week31/01.고층건물/Main.java
+++ 김지원/week31/01.고층건물/Main.java
@@ -7,17 +7,15 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        int n = Integer.parseInt(st.nextToken());
-        int s = Integer.parseInt(st.nextToken());
-        int[] nums = new int[n];
-        st = new StringTokenizer(br.readLine(), " ");
+        int n = Integer.parseInt(br.readLine());
+        int[] buildings = new int[n];
 
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         for (int i = 0; i < n; i++) {
-            nums[i] = Integer.parseInt(st.nextToken());
+            buildings[i] = Integer.parseInt(st.nextToken());
         }
 
-        int result = new Main().solution(n, s, nums);
+        int result = new Main().solution(n, buildings);
 
         bw.write(String.valueOf(result));
         br.close();
@@ -25,27 +23,37 @@ public class Main {
         bw.close();
     }
 
-    public int solution(int n, int s, int[] nums) {
-        int left = 0, right = 0, sum = 0;
+    private int solution(int n, int[] buildingHeights) {
+        int maxCount = 0;
 
-        int answer = Integer.MAX_VALUE;
-        while (true) {
-            if (sum >= s) {
-                sum -= nums[left];
-                left++;
-            } else if (right == n) { // sum < M
-                break;
-            } else { // sum < M && right != n
-                sum += nums[right];
-                right++;
-            }
+        for (int i = 0; i < n; i++) {
+            maxCount = Math.max(calculate(i, n, buildingHeights), maxCount);
+        }
 
-            if (sum >= s) {
-                // (left, right-1)의 길이
-                answer = Math.min(right - left, answer);
+        return maxCount;
+    }
+
+    private int calculate(int cur, int n, int[] buildings) {
+        // right
+        double maxSlope = 0;
+        int visible = 0;
+        for (int i = cur + 1; i < n; i++) {
+            double slope = (buildings[i] - buildings[cur]) / (double) (i - cur);
+            if (i == cur + 1 || slope > maxSlope) {
+                visible++;
+                maxSlope = slope;
             }
         }
 
-        return answer != Integer.MAX_VALUE ? answer : 0;
+        // left
+        for (int i = cur - 1; i >= 0; i--) {
+            double slope = (buildings[i] - buildings[cur]) / (double) (cur - i);
+            if (i == cur - 1 || slope > maxSlope) {
+                visible++;
+                maxSlope = slope;
+            }
+        }
+
+        return visible;
     }
 }
